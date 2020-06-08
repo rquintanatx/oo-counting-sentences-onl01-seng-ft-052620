f60601e9e82baf5e392bfbfe214736aabcd35a17
Robert Quintana <robertquintanatx@gmail.com>
2020-06-08 08:51:52 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,8 +17,7 @@ class String
   def count_sentences
     sentence_array = self.split(/\.|\?|\!/)
     filtered_sentence_array = sentence_array.reject {|i| i.empty?}
-   
-    puts filtered_sentence_array.count
+    filtered_sentence_array.count
   end
 end
 
