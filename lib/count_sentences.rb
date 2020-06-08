0fc0d10538b023436a2d12f9cdd88c198daee3a7
Robert Quintana <robertquintanatx@gmail.com>
2020-06-08 08:35:26 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
     sentence_array = self.split()
-    filtered_sentence_array = sentence_array.select{|sentence| sentence != " "}
+    filtered_sentence_array = sentence_array
     puts filtered_sentence_array.count
   end
 end
